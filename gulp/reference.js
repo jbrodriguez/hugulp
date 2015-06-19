61e1dac6689a2442c544f2ada4f89abcce59c14c
Martin Gladdish <github@martingladdish.co.uk>
2015-06-19 14:51:15 +0100

    Update reference.js
    Image references in css files weren't being replaced with their fingerprinted reference

--- gulp/reference.js
+++ gulp/reference.js
@@ -5,7 +5,7 @@ var size = require('gulp-size');
 gulp.task('reference:content', ['hugo:draft'], function() {
     var manifest = gulp.src('public/rev-manifest.json');
 
-    return gulp.src(['public/**/*.html', 'public/**/*.xml'])
+    return gulp.src(['public/**/*.html', 'public/**/*.xml', 'public/**/*.css'])
         .pipe(replace({manifest: manifest}))
         .pipe(size())
         .pipe(gulp.dest('public'));
@@ -14,8 +14,8 @@ gulp.task('reference:content', ['hugo:draft'], function() {
 gulp.task('reference:all', ['hugo:all'], function() {
     var manifest = gulp.src('public/rev-manifest.json');
 
-    return gulp.src(['public/**/*.html', 'public/**/*.xml'])
-        .pipe(replace({manifest: manifest, replaceInExtensions: ['.html', '.xml']}))
+    return gulp.src(['public/**/*.html', 'public/**/*.xml', 'public/**/*.css'])
+        .pipe(replace({manifest: manifest, replaceInExtensions: ['.html', '.xml', '.css']}))
         .pipe(size())
         .pipe(gulp.dest('public'));
 });
@@ -23,8 +23,8 @@ gulp.task('reference:all', ['hugo:all'], function() {
 gulp.task('reference:publish', ['hugo:live'], function() {
     var manifest = gulp.src('public/rev-manifest.json');
 
-    return gulp.src(['public/**/*.html', 'public/**/*.xml'])
-        .pipe(replace({manifest: manifest, replaceInExtensions: ['.html', '.xml']}))
+    return gulp.src(['public/**/*.html', 'public/**/*.xml', 'public/**/*.css'])
+        .pipe(replace({manifest: manifest, replaceInExtensions: ['.html', '.xml', '.css']}))
         .pipe(size())
         .pipe(gulp.dest('public'));
 });
