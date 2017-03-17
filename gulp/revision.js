9f7cbbf64e2bb23342f80ff94413ca930d1a7359
Jay Dreyer <jay.dreyer@gmail.com>
2017-03-17 16:56:57 -0500

    Add subdirectories to resource paths
    Currently subdirectories of folders like img, scripts and styles
    are not processed because hugulp is expecting a flat file structure.
    Adding /**/*.* will cause gulp to search subdirectories.

--- gulp/revision.js
+++ gulp/revision.js
@@ -4,10 +4,10 @@ var del = require('rev-del');
 var path = require('path');
 
 gulp.task('revision', ['styles','scripts', 'images', 'svg'], function() {
-    return gulp.src(['staging/css/*.css', 'staging/js/*.js', 'staging/img/*.*', 'staging/svg/*.svg'], {base: path.join(process.cwd(), 'staging')})
+    return gulp.src(['staging/css/**/*.css', 'staging/js/**/*.js', 'staging/img/**/*.*', 'staging/svg/**/*.svg'], {base: path.join(process.cwd(), 'staging')})
         .pipe(rev())
         .pipe(gulp.dest('static'))
         .pipe(rev.manifest())
-        .pipe(del({dest: 'static'}))       
+        .pipe(del({dest: 'static'}))
         .pipe(gulp.dest('static'));
 });
