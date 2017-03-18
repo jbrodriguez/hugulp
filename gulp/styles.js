2bf3317a46acde6066437406b881dcbcefe4b0f1
Shawn McElroy <smcelroy@riskalyze.com>
2017-03-17 22:30:46 -0700

    add less support

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,11 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleancss = require('gulp-clean-css');
+var less = require('gulp-less');
+var merge = require('merge-stream');
+var concat = require('gulp-concat');
+
+var lessStream = gulp.src(['assets/styles/*.less'])
+    .pipe(less())
+    .pipe(concat('less-files.less'));
+
+var scssStream = gulp.src(['assets/styles/*.scss'])
+    .pipe(sass())
+    .pipe(concat('scss-files.scss'));
+
+var cssStream = gulp.src(['assets/styles/*.css'])
+    .pipe(concat('css-files.css'));
 
 gulp.task('styles', function() {
-    return gulp.src(['assets/styles/*.scss', 'assets/styles/*.css'])
-        .pipe(sass())
+    return merge(lessStream, scssStream, cssStream)
         .pipe(autoprefixer('last 2 version'))
         .pipe(cleancss({advanced:false}))
+        .pipe(concat('styles.css'))
         .pipe(gulp.dest('staging/css'));
+
 });
