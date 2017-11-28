67be94d9c360c3a9f34899334293639b886a7f68
saschalalala <saschalalala@users.noreply.github.com>
2017-11-28 20:01:36 +0100

    Update vars and coding style
      - See #33

--- gulp/bundle.js
+++ gulp/bundle.js
@@ -1,3 +1,3 @@
-var gulp = require("gulp");
+const gulp = require('gulp');
 
-gulp.task('bundle', ['build:publish']);
+gulp.task('bundle', ['build:publish'])
--- gulp/images.js
+++ gulp/images.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var changed = require('gulp-changed');
+var gulp = require('gulp')
+var imagemin = require('gulp-imagemin')
+var changed = require('gulp-changed')
 
 gulp.task('images', function () {
   return gulp.src('assets/img/**/*.*')
     .pipe(changed('staging/img'))
     .pipe(imagemin())
-    .pipe(gulp.dest('staging/img'));
-});
+    .pipe(gulp.dest('staging/img'))
+})
